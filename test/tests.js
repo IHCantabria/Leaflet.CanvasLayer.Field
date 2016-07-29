ac4952be5db3eddc52f5984ab3d9c3b39ec1e92a
Víctor Velarde <victor.velarde@gmail.com>
2016-07-29 14:13:09 +0200

    Avances en tests básicos, para guardar datos vectoriales

--- test/tests.js
+++ test/tests.js
@@ -2,44 +2,90 @@
     Malla mundo, con datos cada 45º
 */
 "use strict";
-var datosMundo45 = {
+var datosMundo90 = {
     x0: -180.0, // xmin
     y0: -90.0, // ymin
     x1: 180.0, // xmax
-    y1: 90, // ymax
-    dx: 45.0, // incremento en x
-    dy: 45.0, // incremento en y
+    y1: 90.0, // ymax
+    dx: 90.0, // incremento en x
+    dy: 90.0, // incremento en y
     ncols() {
-        return (this.x1 - this.x0) / this.dx;
+        return (this.x1 - this.x0) / this.dx + 1;
     },
     nfilas() {
-        return (this.y1 - this.y0) / this.dy;
+        return (this.y1 - this.y0) / this.dy + 1;
     },
     nVectores() {
         return this.ncols() * this.nfilas();
     },
-    valorEnLonLat(lon, lat) {
-        return -1;
+    UV_LonLat(lon, lat) {
+        var x = this._xLon(lon);
+        var y = this._yLat(lat);
+        return [x, y];
     },
-    campo: [1.0]
-};
+    /**
+     * Índice de fila para Latitud, desde 0
+     * @param   {float} lat Latitud EPSG:4326
+     * @returns {integer} Índice de fila donde buscar el valor de campo
+     */
+    xLat(lat) {
+        var indiceX = (lat - this.y0) / this.dy;
+        return indiceX;
+    },
 
-QUnit.test("hello test", function (assert) {
-    assert.ok(1 == "1", "Passed!");
-});
+    /**
+     * Índice de columna para Longitud, desde 0
+     * @param   {float} lon Longitud EPSG:4326
+     * @returns {integer} Índice de columna donde buscar el valor de campo
+     */
+    yLon(lon) {
+        var indiceY = (lon - this.x0) / this.dx;
+        return indiceY;
+    },
+    /*
+        ascendente-x, una fila por longitud
+        [0, 0] --> coords (-180,-90)
+        [0, 1] --> coords (-90,-90)
+        [0, 2] --> coords (0,-90)
+        ...
+    */
+    campo: [
+        [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]], // (x, -90)
+        [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]], // (x, 0)
+        [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]] //  (x, 90)
+    ]
+};
 
 
 // Datos vectoriales
 QUnit.test("datos - filas y columnas", function (assert) {
-    var datos = datosMundo45;
+    var datos = datosMundo90;
     assert.ok(datos.x0 === -180.0, "x0");
     assert.ok(datos.y0 === -90.0, "y0");
-    assert.equal(datos.ncols(), 8, "columnas");
-    assert.equal(datos.nfilas(), 4, "filas");
-    assert.equal(datos.nVectores(), 32, "n vectores");
+    assert.equal(datos.ncols(), 5, "columnas");
+    assert.equal(datos.nfilas(), 3, "filas");
+    assert.equal(datos.nVectores(), 15, "n vectores");
 });
 
-QUnit.test("datos - valor en posición", function (assert) {
-    var datos = datosMundo45;
-    assert.equal(datos.valorEnLonLat(-180, -90), 0.0, "valor en esquina");
+QUnit.test("datos - índice fila latitud Y", function (assert) {
+    var datos = datosMundo90;
+    assert.equal(datos.xLat(-90), 0, "fila para latitud -90");
+    assert.equal(datos.xLat(0), 1, "fila para latitud 0");
+    assert.equal(datos.xLat(90), 2, "fila para latitud 90");
 });
+
+QUnit.test("datos - índice columna longitud X", function (assert) {
+    var datos = datosMundo90;
+    assert.equal(datos.yLon(-180), 0, "columna para longitud -180");
+    assert.equal(datos.yLon(-90), 1, "columna para longitud -90");
+    assert.equal(datos.yLon(0), 2, "columna para longitud 0");
+    assert.equal(datos.yLon(90), 3, "columna para longitud 90");
+    assert.equal(datos.yLon(180), 4, "columna para longitud 180");
+});
+
+/*
+QUnit.test("datos - valores (U,V) en posición", function (assert) {
+    var datos = datosMundo90;
+    assert.deepEqual(datos.UV_LonLat(-180, -90), [0, 0], "valores UV en esquina inferior izquierda");
+    assert.deepEqual(datos.UV_LonLat(180, 90), [0.5, 0.5], "valores UV en esquina superior derecha");
+});*/
