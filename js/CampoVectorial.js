2543e113ccfb6a4ba791b4f71efea94d68d98a4d
Víctor Velarde <victor.velarde@gmail.com>
2016-08-04 14:43:46 +0200

    En proceso de diseño de la estructura de datos y los métodos para consultar los vectores...

--- js/CampoVectorial.js
+++ js/CampoVectorial.js
@@ -1,12 +1,11 @@
 class CampoVectorial {
-    constructor(x0, y0, x1, y1, dxy, campo) {
+    constructor(x0, y0, x1, y1, dxy) {
         this.x0 = x0;
         this.y0 = y0;
         this.x1 = x1;
         this.y1 = y1;
         this.dx = dxy;
         this.dy = dxy;
-        //        this.campo = campo;
 
         this.componenteU = [];
         this.componenteV = [];
@@ -38,6 +37,19 @@ class CampoVectorial {
         return valido ? [u, v] : null;
     }
 
+
+    /**
+     * Devuelve las coordenadas del vector en el índice indicado
+     * @param   {Integer} índice
+     * @returns {Array} [lon, lat];
+     */
+    lonLatEnIndice(i) {
+        var iCols = this.ncols() - 1;
+        var iFilas = this.nfilas() - 1;
+
+        return [0, 0];
+    }
+
     /**
      * Valor en las coordenadas longitud-latitud
      * @param   {[[Type]]} lon [[Description]]
@@ -45,18 +57,15 @@ class CampoVectorial {
      * @returns {Array}   [u, v]
      */
     valorEn(lon, lat) {
-        // Los datos se almacenan en por filas (x-ascendente) y luego y-ascendente
-
+        // Los datos se almacenan en x-ascendente y luego y-ascendente
         var posy = (lat - this.y0) / this.dy * this.ncols();
         var posx = (lon - this.x0) / this.dx;
 
         return this.vector(posy + posx);
-        /*
-        var x = this._filaLat(lat);
-        var y = this._columnaLon(lon);
+    }
 
-        return this.campo[x][y];
-        */
+    tieneValorEn(lon, lat) {
+        return this.valorEn(lon, lat) !== null;
     }
 
     posicionAleatoria(o = {}) {
@@ -66,32 +75,12 @@ class CampoVectorial {
             x = Math.round(_.random(this.x0, this.x1));
             y = Math.round(_.random(this.y0, this.y1));
         } while (!this.tieneValorEn(x, y) && safetyNet++ < 30);
+
+
         o.x = x;
         o.y = y;
         return o;
     }
 
-    tieneValorEn(lon, lat) {
-        return this.valorEn(lon, lat) !== null;
-    }
 
-    /**
-     * Índice de fila para Latitud, desde 0
-     * @param   {float} lat Latitud EPSG:4326
-     * @returns {integer} Índice de fila donde buscar el valor de campo
-     */
-    _filaLat(lat) {
-        var indiceX = (lat - this.y0) / this.dy;
-        return Math.round(indiceX);
-    }
-
-    /**
-     * Índice de columna para Longitud, desde 0
-     * @param   {float} lon Longitud EPSG:4326
-     * @returns {integer} Índice de columna donde buscar el valor de campo
-     */
-    _columnaLon(lon) {
-        var indiceY = (lon - this.x0) / this.dx;
-        return Math.round(indiceY);
-    }
 }
--- tests/spec/CampoVectorialSpec.js
+++ tests/spec/CampoVectorialSpec.js
@@ -4,45 +4,13 @@ describe("CampoVectorial", function () {
         componenteU = [0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0.9];
         componenteV = [0, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0, 0.9];
 
-        campo = null;
-        /*[
-            [
-                [0, 0],
-                [0, 0],
-                [0, 0],
-                [0, 0],
-                [0, 0]
-            ], // (x, -90)
-            [
-                [0, 0],
-                [0, 0],
-                [0.5, 0.5],
-                [0, 0],
-                [0, 0]
-            ], // (x, 0)
-            [
-                [0, 0],
-                [0, 0],
-                [0, 0],
-                [0, 0],
-                [0.9, 0.9]
-            ] //  (x, 90)
-            ];
-        */
-
-        campoVectorial = new CampoVectorial(-180, -90, 180, 90, 90, campo);
+        campoVectorial = new CampoVectorial(-180, -90, 180, 90, 90);
         campoVectorial.componenteU = componenteU;
         campoVectorial.componenteV = componenteV;
 
     });
 
-    it("incluye ambos componentes, u y v", function () {
-        expect(campoVectorial.componenteU.length).toBeGreaterThan(0);
-        expect(campoVectorial.componenteV.length).toBeGreaterThan(0);
-    });
-
-
-    it("debería dar información de su extensión", function () {
+    it("debería dar información general sobre su extensión", function () {
         expect(campoVectorial.x0).toEqual(-180.0);
         expect(campoVectorial.y0).toEqual(-90.0);
         expect(campoVectorial.ncols()).toEqual(5);
@@ -50,46 +18,39 @@ describe("CampoVectorial", function () {
         expect(campoVectorial.nVectores()).toEqual(15);
     });
 
+    it("permite obtener todos los componentes U & V", function () {
+        expect(campoVectorial.componenteU.length).toBeGreaterThan(0);
+        expect(campoVectorial.componenteV.length).toBeGreaterThan(0);
+    });
 
-    /*
-    /*
-        it("debería recuperar la fila para una latitud Y", function () {
-            expect(campoVectorial.filaLat(-90)).toEqual(0);
-            expect(campoVectorial.filaLat(0)).toEqual(1);
-            expect(campoVectorial.filaLat(90)).toEqual(2);
-        });
-
-        it("debería recuperar la columna para una longitud X", function () {
-            expect(campoVectorial.columnaLon(-180)).toEqual(0);
-            expect(campoVectorial.columnaLon(-90)).toEqual(1);
-            expect(campoVectorial.columnaLon(0)).toEqual(2);
-            expect(campoVectorial.columnaLon(90)).toEqual(3);
-            expect(campoVectorial.columnaLon(180)).toEqual(4);
-        });
-    */
-
-    it("debería devolver los vectores", function () {
+    it("debería devolver el vector en una posición", function () {
         expect(campoVectorial.nVectores()).toEqual(15);
         expect(campoVectorial.vector(0)).toEqual([0, 0]);
         expect(campoVectorial.vector(14)).toEqual([0.9, 0.9]);
     });
 
-    it("debería devolver el vector en una posición Lon-Lat", function () {
+    it("debería conocer la Lon-Lat de posición", function () {
+        expect(campoVectorial.lonLatEnIndice(0)).toEqual([-180, -90]);
+        expect(campoVectorial.lonLatEnIndice(14)).toEqual([180, 90]);
+    });
+
+    it("debería devolver el vector en una Lon-Lat", function () {
         expect(campoVectorial.valorEn(-180, -90)).toEqual([0, 0]); // LL
         expect(campoVectorial.valorEn(0, 0)).toEqual([0.5, 0.5]); // ecuador-greenwhich
         expect(campoVectorial.valorEn(180, 90)).toEqual([0.9, 0.9]); // UR
+
+        expect(campoVectorial.valorEn(-360, -180)).toEqual(null);
     });
 
-    /*
+    it("debería permitir saber si una posición tiene valor", function () {
+        expect(campoVectorial.tieneValorEn(0, 0)).toBe(true);
+        expect(campoVectorial.tieneValorEn(-1, -1)).toBe(false);
+        expect(campoVectorial.tieneValorEn(-179, -89)).toBe(false);
+    });
 
-        it("debería permitir saber si una posición tiene valor", function () {
-            expect(campoVectorial.tieneValorEn(0, 0)).toBe(true);
-            expect(campoVectorial.tieneValorEn(-1, -1)).toBe(false);
-        });
-
-
-        it("debería poder devolver una posición aleatoria dentro", function () {
-            expect(campoVectorial.posicionAleatoria()).not.toBe(null);
-        });
-        */
+    it("debería poder devolver una posición aleatoria dentro", function () {
+        var pos = campoVectorial.posicionAleatoria();
+        expect(pos).not.toBe(null);
+        expect(campoVectorial.tieneValorEn(pos.x, pos.y)).toBe(true);
+    });
 });
