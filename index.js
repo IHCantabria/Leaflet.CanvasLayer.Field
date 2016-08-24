8e5d50921d6c8711b43ef4426eb65f98d8ebba6f
Víctor Velarde <victor.velarde@gmail.com>
2016-08-24 10:17:02 +0200

    idem

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 const mapa = L.map("map").setView([43.45, -3.7944], 13);
 
 /* Base */
-//let url = 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png';
-let url = 'http://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png';
+let url = 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png';
+//let url = 'http://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png';
 L.tileLayer(url, {
     attribution: 'OSM & Carto',
     subdomains: 'abcd',
@@ -14,44 +14,40 @@ L.tileLayer(url, {
 
 
 /* Capas animadas */
-d3.json("data/grid_CANTABRIA.json", function (d) {
+d3.json("data/grid_BAHIA.json", function (d) {
     let cv = CampoVectorial.desdeJson(d);
 
     // 0. Grid base
-    let capaGrid = new CapaPuntos(cv.mallaLonLatUV());
-    //capaGrid.addTo(mapa);
+    L.capaPuntos(cv.mallaLonLatUV()); //.addTo(mapa);
 
     // 1. Básica
-    let capa = new L.CapaVectorAnimado(cv);
-    //capa.addTo(mapa);
+    L.capaVectorAnimado(cv); //.addTo(mapa);
 
     // 2. Cambio de color
-    let capa2 = new L.CapaVectorAnimado(cv, {
+    L.capaVectorAnimado(cv, {
         color: "green"
-    });
-    //capa2.addTo(mapa);
+    }); //.addTo(mapa);
 
     // 3. Más parámetros personalizados
-    let capa3 = new L.CapaVectorAnimado(cv, {
+    L.capaVectorAnimado(cv, {
+        tr
         trayectorias: 150,
-        duracion: 20,
-        edadMaxima: 200,
-        color: "yellow",
-        grosor: 3
-    });
-    //capa3.addTo(mapa);
+            duracion: 10,
+            edadMaxima: 200,
+            color: "#FF6699",
+            grosor: 8
+    }); // .addTo(mapa);
 
     // 4. Capa con color por velocidad y leyenda asociada
     var m = MapaColor.paraCorrientes([0, 1.1]);
     let capa4 = new L.CapaVectorAnimado(cv, {
         color: m.escala
     });
-    capa4.addTo(mapa);
-    //mapa.fitBounds(capa4.getBounds());
+    //capa4.addTo(mapa);
 
     L.Control.leyendaEscalaColor(m).addTo(mapa);
 
-    // 5. Identificación con click
+    // 6. Identificación con click
     capa4.on('click_vector', function (e) {
         if (e.vector) {
             let v = e.vector.longitud().toFixed(3);
@@ -62,6 +58,7 @@ d3.json("data/grid_CANTABRIA.json", function (d) {
                 .setContent(html)
                 .openOn(mapa);
         }
-
     });
+
+    //mapa.fitBounds(capa4.getBounds());
 });
