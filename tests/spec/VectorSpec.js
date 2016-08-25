b864a99a8bdac9d45ed64eda41513301c025ac27
Víctor Velarde <victor.velarde@gmail.com>
2016-08-25 14:46:33 +0200

    -

--- tests/spec/VectorSpec.js
+++ tests/spec/VectorSpec.js
@@ -3,25 +3,22 @@ describe("Vector", function () {
     beforeEach(function () {
         vector = new Vector(0.0, 0.1);
     });
-    it("debería permitir recuperar sus componentes x-y", function () {
+    it("returns u & v values", function () {
         expect(vector.u).toEqual(0.0);
         expect(vector.v).toEqual(0.1);
     });
-    it("debería calcular su longitud", function () {
+    it("calculates its magnitude", function () {
         expect(vector.magnitude()).toEqual(0.1);
 
-        /*
-         porcion.json esquina superior derecha
-         velocidad calculada por GDAL calc (QGIS) = 0.215025
-         */
+        // upper-right corner @ test.json
         let v = new Vector(0.215018898248672, -0.00158081843983382);
         expect(v.magnitude()).toBeCloseTo(0.215025, 6);
 
     });
-    it("debería calcular su ángulo (en radianes)", function () {
+    it("calculates its direction (radians)", function () {
         expect(vector.direction()).toBeCloseTo(1.57, 2);
     });
-    it("debería calcular su ángulo (en grados)", function () {
+    it("calculates its direction (degrees)", function () {
         expect(vector.directionDeg()).toEqual(90.0);
     });
 });
