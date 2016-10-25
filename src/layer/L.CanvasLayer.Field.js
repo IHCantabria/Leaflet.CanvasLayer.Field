a3277414cd759e71f2beefe233a3e058196e4e86
Víctor Velarde <victor.velarde@gmail.com>
2016-10-25 13:45:16 +0200

    Preparing a refactoring, to share some methods between ScalarField and VectorField, through a Base class

--- src/layer/L.CanvasLayer.Field.js
+++ src/layer/L.CanvasLayer.Field.js
@@ -1,10 +1,62 @@
 /**
- *  Abstract class for a set of values (Vector | Scalar)
- *  assigned to a regular 2D-grid (lon-lat), aka "a Raster layer"
+ * Abstract class for a Field layer on canvas, aka "a Raster layer"
+ * (ScalarField | VectorField)
  */
-class Field {
+L.CanvasLayer.Field = L.CanvasLayer.extend({
+    options: {
+        click: true, // 'click' event
+    },
 
-    constructor(params) {
-        if (new.target === Field) {
-            throw new TypeError("Cannot construct Field instances directly (use VectorField or ScalarField)");
+    initialize: function (field, options) {
+        // TODO protect against direct instantiation!!
+
+        this.field = field;
+        L.Util.setOptions(this, options);
+    },
+
+    onLayerDidMount: function () {
+        if (this.options.click) {
+            this._map.on('mouseover', this._activateClick, this);
+            this._map.on('click', this._queryValue, this);
         }
+    },
+
+    onLayerWillUnmount: function () {
+        if (this.options.click) {
+            this._map.off('mouseover', this._activateClick, this);
+            this._map.off('click', this._queryValue, this);
+        }
+    },
+
+    setData: function (data) {
+        // -- custom data set
+        // TODO
+        this.needRedraw(); // -- call to drawLayer
+    },
+
+    onDrawLayer: function (viewInfo) {
+        throw new TypeError("Must be overriden");
+    },
+
+    getBounds: function () {
+        let bb = this.field.extent();
+        let southWest = L.latLng(bb[1], bb[0]),
+            northEast = L.latLng(bb[3], bb[2]);
+        let bounds = L.latLngBounds(southWest, northEast);
+        return bounds;
+    },
+
+    _activateClick: function () {
+        this._map.getContainer().style.cursor = 'default';
+    },
+
+    _queryValue: function (e) {
+        let lon = e.latlng.lng;
+        let lat = e.latlng.lat;
+        let result = {
+            latlng: e.latlng,
+            value: this.field.valueAt(lon, lat)
+        };
+        this.fireEvent('click', result); /*includes: L.Mixin.Events,*/
+    }
+});
