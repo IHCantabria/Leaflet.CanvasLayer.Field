df87b9194ab0b3a8ec578e35d1dbbe90d8557222
Víctor Velarde <victor.velarde@gmail.com>
2017-04-04 11:02:56 +0200

    Indicating tag and source for the plugin

--- src/layer/L.CanvasLayer.js
+++ src/layer/L.CanvasLayer.js
@@ -1,8 +1,9 @@
 /*
+  1.0.1 (downloaded from https://github.com/Sumbera/gLayers.Leaflet/releases/tag/v1.0.1)
+
   Generic  Canvas Layer for leaflet 0.7 and 1.0-rc,
   copyright Stanislav Sumbera,  2016 , sumbera.com , license MIT
   originally created and motivated by L.CanvasOverlay  available here: https://gist.github.com/Sumbera/11114288
-
 */
 
 L.CanvasLayer = L.Layer.extend({
