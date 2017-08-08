3add033d46e9221cc113ad1a2e58f743cdcfd915
Víctor Velarde <victor.velarde@gmail.com>
2017-08-08 21:12:42 +0200

    more tests

--- spec/VectorSpec.js
+++ spec/VectorSpec.js
@@ -1,21 +1,25 @@
-describe('Vector', function () {
+describe('Vector', function() {
+    // For reference, this is a great library: http://victorjs.org/
     let Vector = L.Vector;
-    it('returns u & v values', function () {
+    it('returns u & v values', function() {
         let vector = new Vector(0.0, 0.1);
         expect(vector.u).toEqual(0.0);
         expect(vector.v).toEqual(0.1);
     });
 
-    it('calculates its magnitude', function () {
+    it('calculates its magnitude', function() {
         let vector = new Vector(0.0, 0.1);
         expect(vector.magnitude()).toEqual(0.1);
+
+        vector = new Vector(100, 100);
+        expect(vector.magnitude()).toBeCloseTo(141.42, 2);
 
         // upper-right corner @ test.json
         let v = new Vector(0.215018898248672, -0.00158081843983382);
         expect(v.magnitude()).toBeCloseTo(0.215025, 6);
     });
 
-    it('calculates its directionTo / directionFrom', function () {
+    it('calculates its directionTo / directionFrom', function() {
         expect(new Vector(0, 1).directionTo()).toEqual(0);
         expect(new Vector(0, 1).directionFrom()).toEqual(180);
 
@@ -39,5 +43,13 @@ describe('Vector', function () {
 
         expect(new Vector(-1, 1).directionTo()).toEqual(315);
         expect(new Vector(-1, 1).directionFrom()).toEqual(135);
+
+        vector = new Vector(100, 100);
+        expect(vector.directionTo()).toEqual(45);
+        expect(vector.directionFrom()).toEqual(225);
+
+        vector = new Vector(100, 0);
+        expect(vector.directionTo()).toEqual(90);
+        expect(vector.directionFrom()).toEqual(270);
     });
 });
